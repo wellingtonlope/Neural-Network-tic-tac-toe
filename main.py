7d3d25f5f9e7558a462a0dac16cc51d24231fe68
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-11-19 15:38:22 -0400

    add comments

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from algorithm import readFile, separator, trainer
+#leitura e conversão dos dados
 data = readFile('data/tic-tac-toe.data')
 
+#separação dos dados de treinamento e teste
 dataTrain, dataTest = separator(data, 320)
 
+# treinamento e obtenção dos resultados da rede neural
+# trainer(dadosDeTreinamento, dadosDeTeste, neuroniosNaCamadaOculta, taxaDeAprendizado, geracoes)
 result = trainer(dataTrain, dataTest, 75, 0.4, 100)
 print(result.treino)
 print(result.teste)
